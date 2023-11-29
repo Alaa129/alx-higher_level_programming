8ef6f5a519273d753800204a0f80d42dcfb78147
Alaa129 <alaa1292002.a@gmail.com>
2023-11-29 03:16:29 +0200

    13-insert_number.c

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,74 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
- * print_listint - print_listint
- * @h: pointer
- * Return: number
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
  */
-size_t print_listint(const listint_t *h)
+typedef struct listint_s
 {
-	const listint_t *curr;
-	unsigned int n;
+    int n;
+    struct listint_s *next;
+} listint_t;
 
-	curr = h;
-	n = 0;
-	while (curr != NULL)
-	{
-		printf("%i\n", curr->n);
-		curr = curr->next;
-		n++;
-	}
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
-	return (n);
-}
+listint_t *insert_node(listint_t **head, int number);
 
-/**
- * add_nodeint_end - add_nodeint_end
- * @head: pointer
- * @n: num
- * Return: 0
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-	listint_t *nas;
-	listint_t *cur;
-
-	cur = *head;
-
-	nas = malloc(sizeof(listint_t));
-	if (nas == NULL)
-		return (NULL);
-
-	nas->n = n;
-	nas->next = NULL;
-
-	if (*head == NULL)
-		*head = nas;
-	else
-	{
-		while (cur->next != NULL)
-			cur = cur->next;
-		cur->next = nas;
-	}
-
-	return (nas);
-}
-
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-	listint_t *current;
-
-	while (head != NULL)
-	{
-		current = head;
-		head = head->next;
-		free(current);
-	}
-}
+#endif /* LISTS_H */
